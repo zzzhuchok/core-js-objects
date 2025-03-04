5e86c91001aa79200fc0152aebe06d4ac80ab474
zzzhuchok <zzzhuchok@gmail.com>
2025-03-04 12:48:38 +0300

    feat: add solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,8 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  return { ...Object.assign(obj) };
 }
 
 /**
@@ -32,8 +32,17 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const resultObj = {};
+
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      resultObj[key] = Object.hasOwn(resultObj, key)
+        ? resultObj[key] + value
+        : value;
+    });
+  });
+  return resultObj;
 }
 
 /**
@@ -49,8 +58,14 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const objCopy = { ...obj };
+  keys.forEach((key) => {
+    if (Object.hasOwn(objCopy, key)) {
+      delete objCopy[key];
+    }
+  });
+  return objCopy;
 }
 
 /**
@@ -65,8 +80,19 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (let i = 0; i < keys1.length; i += 1) {
+    if (!keys2.includes(keys1[i]) || obj1[keys1[i]] !== obj2[keys1[i]]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
@@ -80,8 +106,8 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  return Object.keys(obj).length < 1;
 }
 
 /**
